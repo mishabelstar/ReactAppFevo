d40e15c9207bf4127546f382c8a25f856d0bf62d
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2018-11-06 15:23:28 -0500

    comment here

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
 Just pick your event, from concerts to sports games, and invite your friends. Then they invite their friends and everyone buys their own tickets. Fevo handles the rest, including seating you all together.
 Your tickets always come directly from inventory owners, because Fevo partners with the industry's leading primary ticket sellers and venues to create a best-in-class experience.
 </p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Next</Link>
   </div>
 )
 
