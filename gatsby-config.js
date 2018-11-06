ca2612c790326a5fc4d3d1b285764128c3edb970
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2018-11-06 15:17:26 -0500

    init

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Welcome to Fevo',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Better Together</h1>
+    <p>How it works
+Just pick your event, from concerts to sports games, and invite your friends. Then they invite their friends and everyone buys their own tickets. Fevo handles the rest, including seating you all together.
+Your tickets always come directly from inventory owners, because Fevo partners with the industry's leading primary ticket sellers and venues to create a best-in-class experience.
+</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
